686d4e4ca502199acebddb55fb84f440b5b9ec89
psicopedagogiaam26-create <psicopedagogiaam26@gmail.com>
2026-04-14 16:43:52 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// ── Reemplazá estos valores con los de tu proyecto Firebase ──
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROJECT.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_PROJECT.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
